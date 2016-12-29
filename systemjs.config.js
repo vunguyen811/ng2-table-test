018b485a0e9a79aed334efc92763515f415256d3
vunguyen <vu.nguyen@newoceaninfosys.com>
2016-12-29 17:13:31 +0700

    fix ng2-table

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,7 +25,8 @@
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
             'ng2-table': 'npm:ng2-table',
-            'ng2-bootstrap/ng2-bootstrap': 'npm:ng2-bootstrap/bundles/ng2-bootstrap.js',
+            'ng2-bootstrap': 'npm:ng2-bootstrap',
+            'moment': 'npm:moment/min/moment.min.js',
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -41,7 +42,10 @@
                 defaultExtension: 'js'
             },
             'ng2-table': {
-                defaultExtension: 'js'  
+                defaultExtension: 'js'
+            },
+            'ng2-bootstrap': {
+                defaultExtension: 'js'
             }
         }
     });
